3535aa91c46cd04b2339a3b7fbc6d67d73fdfdd5
viniciusoliveira01 <vinicius.alexandre01@hotmail.com>
2018-08-20 20:31:17 -0300

    create team and auth

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,13 +1,20 @@
+import formatErrors from '../formatErrors';
+
 export default {
   Mutation: {
     createTeam: async (parent, args, { models, user }) => {
       try {
-        await models.Team.create({...args, owner: user.id})
-        return true
-      } catch (e) {
-        console.log(e)
-        return false
+        await models.Team.create({ ...args, owner: user.id });
+        return {
+          ok: true,
+        };
+      } catch (err) {
+        console.log(err);
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
       }
-    }
-  }
-}
+    },
+  },
+};
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,37 +1,18 @@
-import bcrypt from 'bcrypt'
-import _ from 'lodash'
-
-const formatErrors = (e, models) => {
-  if (e instanceof models.sequelize.ValidationError) {
-    //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
-    return e.errors.map(x => _.pick(x, ['path', 'message']));
-  }
-  return [{ path: 'name', message: 'something went wrong' }];
-};
+import { tryLogin } from '../auth'
+import formatErrors from '../formatErrors'
 
 export default {
   Query: {
-    getUser: (parent, { id }, { models }) =>
-      models.User.findOne({ where: { id } }),
+    getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {
-    register: async (parent, { password, ...otherArgs }, { models }) => {
+    login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
+      tryLogin(email, password, models, SECRET, SECRET2),
+    register: async (parent, args, { models }) => {
       try {
-        if (password.length < 5 || password.length > 100) {
-          return {
-            ok: false,
-            errors: [
-              {
-                path: 'password',
-                message: 'The password needs to be between 5 and 100 characters long',
-              }
-            ]
-          }
-        }
+        const user = await models.User.create(args);
 
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const user = await models.User.create({ ...otherArgs, password: hashedPassword })
         return {
           ok: true,
           user
@@ -39,7 +20,7 @@ export default {
       } catch (err) {
         return {
           ok: false,
-          errors: formatErrors(err, models)
+          errors: formatErrors(err, models),
         }
       }
     }
